5fc3f1233339e8f62750ce65825fef4cda03f37f
Matt Bradford <bradfmd@gmail.com>
2018-02-06 13:49:39 -0600

    setup work section

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/mbtech_stacked.png'
+import pic02 from '../images/clients.png'
+import pic03 from '../images/mbtech_stacked_1.png'
 
 class Main extends React.Component {
   render() {
@@ -15,8 +15,7 @@ class Main extends React.Component {
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
           <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <p>We work with a wide variety of customers across several industries. Our primary focus is automating business processes and extending ERP systems.</p>
           {close}
         </article>
 
